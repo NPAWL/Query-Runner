a5776556e46cb6c55f49ea01c93f9d8ef366401a
SUPERGOD6 <s216298792@mandela.ac.za>
2018-09-06 18:07:03 +0200

    Finished B0200 and B0800, added B0700. Test Please.

---
+++ QueryRunner/QueryRunner/Models/EditTestAndQuestionsViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;  
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;  
+using Library.Models;
+using QueryRunner.Helpers;
+
+namespace QueryRunner.Models
+  {
+  public class EditTestAndQuestionsViewModel
+    {
+      private ModelTest _model = new ModelTest();
+                     
+      [Required]
+      public int TestID { get { return _model.TestID; } set { _model.TestID = value; } }
+      [Required]
+      public string TestName { get { return _model.TestName; } set { _model.TestName = value; } } 
+      [Required]
+      public string TestUsername { get { return _model.Username; } set { _model.Username = value; } }
+      [Required]
+      public DateTime TestDate { get { return _model.Date; } set { _model.Date = value; } }
+      [Required]
+      public DateTime TestStartTime { get { return _model.StartTime; } set { _model.StartTime = value; } }
+      [Required]
+      public DateTime TestEndTime { get { return _model.EndTime; } set { _model.EndTime = value; } }   
+      [Required]
+      public bool TestActive { get { return _model.TestActive; } set { _model.TestActive = value; } }
+      public List<ModelQuestion> Questions { get; set; }
+      
+      public EditTestAndQuestionsViewModel() {}
+      public EditTestAndQuestionsViewModel(ModelTest model) { _model = model; }  
+      public EditTestAndQuestionsViewModel(ModelTest model, List<ModelQuestion> questions)
+      {
+          _model = model;
+          Questions = questions;
+      }
+      public ModelTest ToDataModel() { return _model; }
+
+    }
+  }
