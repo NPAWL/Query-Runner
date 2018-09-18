409b35791e6f5d9f561dccc27fd649bc013521fe
Valicious <gert.pe.agg@gmail.com>
2018-09-18 19:33:03 +0200

    Updated variable types

--- A0200/SimpleSQLCheck/SimpleSQLCheck/Program.cs
+++ A0200/SimpleSQLCheck/SimpleSQLCheck/Program.cs
@@ -5,10 +5,8 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace SimpleSQLCheck
-{
-    class Program
-    {
+namespace SimpleSQLCheck {
+    class Program {
         /**
          *  1.) Need to incorporate:
          *          SELECT P.ID FROM Person P 
@@ -16,10 +14,9 @@ namespace SimpleSQLCheck
          *          SELECT Person.ID FROM Person
          */
 
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) {
             //Splitting the statement into parts
-            String question =
+            string question =
                 "SELECT Name, Surname, (Age+10) as Age From People where ID = 5 order by Surname ASC Group by Age"; //Correct
             //String question = "SELECT Name, (Age+10) as Age From People where ID = 5 order by Surname ASC Group by Age";//Select
             //String question = "SELECT Name, Surname, (Age+10) as Age From People P where ID = 5 order by Surname ASC Group by Age";//From
@@ -28,17 +25,17 @@ namespace SimpleSQLCheck
             //String question = "SELECT Name, Surname, (Age+10) as Age From People where ID = 5 order by Surname ASC";//group by
 
             //String question = "SELECT Name, Surname, (Age+10) as Age From People where ID = 5 order by Surname ASC Group by Age";
-            String answer =
+            string answer =
                 "SELECT Name, Surname, (Age+10) as Age From People where ID = 5 order by Surname ASC Group by Age";
             answer.Replace("\n", "");
             answer.Replace("\r", "");
             answer.Replace("\t", " ");
             answer.Replace("  ", " ");
             //NO SUBQUERIES! SELECT, FROM, WHERE, ORDER BY, GROUP BY
-            string pattern_SFWOG = @"(.+?(?=from))|(.+(?= where))| (.+(?= order by))| (.+(?= group by))| (.*)";
-            RegexOptions options = RegexOptions.IgnoreCase;
-            List<String> parts_Q = Regex.Split(question, pattern_SFWOG, options).ToList();
-            List<String> parts_A = Regex.Split(answer, pattern_SFWOG, options).ToList();
+            const string pattern_SFWOG = @"(.+?(?=from))|(.+(?= where))| (.+(?= order by))| (.+(?= group by))| (.*)";
+            const RegexOptions options = RegexOptions.IgnoreCase;
+            List<string> parts_Q = Regex.Split(question, pattern_SFWOG, options).ToList();
+            List<string> parts_A = Regex.Split(answer, pattern_SFWOG, options).ToList();
             parts_Q.RemoveAll(cur => cur.Equals(""));
             parts_A.RemoveAll(cur => cur.Equals(""));
 
@@ -49,82 +46,75 @@ namespace SimpleSQLCheck
             parts_A.ToList().ForEach(cur => Console.WriteLine("\t" + cur));
 
             double mark =
-                doSelect(parts_A, parts_Q)
-                + doFrom(parts_A, parts_Q)
-                + doWhere(parts_A, parts_Q)
-                + doGroupBy(parts_A, parts_Q)
-                + doOrderBy(parts_A, parts_Q);
+                DoSelect(parts_A, parts_Q)
+                + DoFrom(parts_A, parts_Q)
+                + DoWhere(parts_A, parts_Q)
+                + DoGroupBy(parts_A, parts_Q)
+                + DoOrderBy(parts_A, parts_Q);
             mark /= 5;
 
-            Console.WriteLine("Final mark is {:2}%", mark);
+            Console.WriteLine("Final mark is {0:2}%", mark * 100);
 
-            Console.ReadLine();
+            // Console.ReadLine();
         }
 
-        private static double doSelect(List<String> answer, List<String> question)
-        {
+        private static double DoSelect(IEnumerable<string> answer, IEnumerable<string> question) {
             //Checking SELECT
-            String SELECT_Q = get("SELECT", question).Replace(" ", "");
-            String SELECT_A = get("SELECT", answer).Replace(" ", "");
-            List<String> attributes_Q = SELECT_Q.Split(',').ToList();
-            List<String> attributes_A = SELECT_A.Split(',').ToList();
-            double count = 0;
-            attributes_Q.ForEach(cur =>
-            {
-                if (attributes_A.Contains(cur)) count++;
+            string selectQ = Get("SELECT", question).Replace(" ", "");
+            string selectA = Get("SELECT", answer).Replace(" ", "");
+            List<string> attributesQ = selectQ.Split(',').ToList();
+            List<string> attributesA = selectA.Split(',').ToList();
+            double count = 0.00;
+            attributesQ.ForEach(cur => {
+                if (attributesA.Contains(cur)) count++;
             });
-            return count;
+            return (count * 1.00) / attributesA.Count();
         }
 
-        private static double doFrom(List<String> answer, List<String> question)
-        {
+        private static double DoFrom(IEnumerable<string> answer, IEnumerable<string> question) {
             //Checking FROM
-            String FROM_Q = get("FROM", question);
-            String FROM_A = get("FROM", answer);
-            List<String> attributes_Q = FROM_Q.Split(',').ToList();
-            List<String> attributes_A = FROM_A.Split(',').ToList();
+            string fromQ = Get("FROM", question);
+            string fromA = Get("FROM", answer);
+            List<string> attributesQ = fromQ.Split(',').ToList();
+            List<string> attributesA = fromA.Split(',').ToList();
             double count = 0;
-            attributes_Q.ForEach(cur =>
-            {
-                if (attributes_A.Contains(cur)) count++;
+            attributesQ.ForEach(cur => {
+                if (attributesA.Contains(cur)) count++;
             });
-            return count;
+            return (count * 1.00) / attributesA.Count();
         }
 
-        private static double doWhere(List<String> answer, List<String> question)
-        {
+        private static double DoWhere(IEnumerable<string> answer, IEnumerable<string> question) {
             //Checking WHERE
-            String WHERE_Q = get("WHERE", question);
-            String WHERE_A = get("WHERE", answer);
+            /*string whereQ = Get("WHERE", question);
+            string whereA = Get("WHERE", answer);*/
             return 0;
         }
 
-        private static double doGroupBy(List<String> answer, List<String> question)
-        {
+        private static double DoGroupBy(IEnumerable<string> answer, IEnumerable<string> question) {
             //Checking ORDER BY
-            String ODBY_Q = get("ORDER BY", question);
-            String ODBY_A = get("ORDER BY", answer);
+            /*
+            string odbyQ = Get("ORDER BY", question);
+            string odbyA = Get("ORDER BY", answer);*/
             return 0;
         }
 
-        private static double doOrderBy(List<String> answer, List<String> question)
-        {
+        private static double DoOrderBy(IEnumerable<string> answer, IEnumerable<string> question) {
             //Checking GROUP BY
-            String GPBY_Q = get("GROUP BY", question);
-            String GPBY_A = get("GROUP BY", answer);
+            /*string GPBY_Q = Get("GROUP BY", question);
+            string GPBY_A = Get("GROUP BY", answer);
 
+*/
 
             return 0;
         }
+
 
         #region [Helpers]
 
-        private static String get(String selector, List<String> list)
-        {
-            foreach (String cur in list)
-            {
-                if (cur.ToUpper().Substring(0, selector.Length).Equals(selector))
-                {
+        private static string Get(string selector, IEnumerable<string> list) {
+            foreach (string cur in list) {
+                if (cur.ToUpper().Substring(0, selector.Length).Equals(selector)) {
                     return cur.Substring(selector.Length, cur.Length - selector.Length);
                 }
             }
@@ -135,10 +125,8 @@ namespace SimpleSQLCheck
         #endregion
     }
 
-    class BadSqlOperation : Exception
-    {
-        public BadSqlOperation(string message) : base(message)
-        {
+    internal sealed class BadSqlOperation : Exception {
+        public BadSqlOperation(string message) : base(message) {
             Console.WriteLine(message);
             Console.WriteLine(StackTrace);
         }
